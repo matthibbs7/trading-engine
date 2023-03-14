189da7c7f7d94cb4f9a534fb065f84701d997604
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2023-03-14 00:47:29 -0400

    Added tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod matching_engine;
 use matching_engine::orderbook::{Order, BidOrAsk, Orderbook};
-use matching_engine::engine::MatchingEngine;
+use matching_engine::engine::{MatchingEngine, TradingPair};
 
 fn main() {
     let buy_order_from_alice = Order::new(BidOrAsk::Bid, 5.5);
@@ -11,6 +11,14 @@ fn main() {
     orderbook.add_order(4.4, buy_order_from_alice);
     orderbook.add_order(4.4, buy_order_from_bob);
     orderbook.add_order(20.0, sell_order);
-    println!("{:?}", orderbook);
-    let engine = MatchingEngine::new();
+    // println!("{:?}", orderbook);
+
+    let mut engine = MatchingEngine::new();
+    let pair = TradingPair::new("BTC".to_string(), "USD".to_string());
+    engine.add_new_market(pair.clone());
+
+    let buy_order = Order::new(BidOrAsk::Bid, 6.5);
+    let eth_pair = TradingPair::new("ETH".to_string(), "USD".to_string());
+    engine.place_limit_order(pair, 10.000, buy_order).unwrap();
+
 }
--- src/matching_engine/engine.rs
+++ src/matching_engine/engine.rs
@@ -1,4 +1,4 @@
-use super::orderbook::{Orderbook};
+use super::orderbook::{Orderbook, Order};
 use std::collections::HashMap;
 
 // ==============================
@@ -7,6 +7,7 @@ use std::collections::HashMap;
 //        USD -> QUOTE
 // ==============================
 
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
 pub struct TradingPair {
     base: String,
     quote: String,
@@ -18,6 +19,10 @@ impl TradingPair {
             base,
             quote
         }
+    }
+
+    pub fn to_string(self) -> String {
+        format!("{}_{}", self.base, self.quote)
     }
 } 
 
@@ -31,4 +36,26 @@ impl MatchingEngine {
             orderbooks: HashMap::new(),
         }
     }
+
+    pub fn add_new_market(&mut self, pair: TradingPair) {
+        self.orderbooks.insert(pair.clone(), Orderbook::new());
+        println!("opening new orderbook for market {:?}", pair.to_string());
+    }
+
+    pub fn place_limit_order(&mut self, pair: TradingPair, price: f64, order: Order) -> Result<(), String> {
+        match self.orderbooks.get_mut(&pair) {
+            Some(orderbook) => {
+                orderbook.add_order(price, order);
+                println!("placed limit order at price level {}", price);
+                Ok(())
+            }
+            None => {
+                Err(format!("the orderbook for the given trading pair ({}) does not exist",
+                pair.to_string()
+            ))
+            }
+        }
+    }
+
+    
 }
